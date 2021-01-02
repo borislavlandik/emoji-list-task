90ad582f6f6c219072f18a7a2e99444358755a17
Борислав Ландик <borislavlandik00@gmail.com>
2021-01-02 14:50:29 +0800

    feat: add emoji list to favorite page

--- src/app/favorite.component.ts
+++ src/app/favorite.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmojiModel} from './emoji.model';
+import {EmojiService} from './emoji.service';
 
 @Component({
   selector: 'favorite-app',
-  template: '<h1>ЛЮБИМЫЕ</h1>'
+  template: `
+    <header class="header">
+      <h1 class="header__title">Любимые</h1>
+      <app-search-box (onInput)="filter = $event"></app-search-box>
+    </header>
+    <ng-container *ngFor="let emoji of emojis | filter:'name':filter.toLowerCase(); index as i; trackBy: trackBy">
+      <app-emoji-item [emoji]="emoji" *ngIf="emoji.isFavorite">
+        <button actions (click)="removeFavorite(emoji.name)">
+          <img src="../assets/icons/close.svg" alt="cross icon">
+        </button>
+      </app-emoji-item>
+    </ng-container>
+  `,
+  styleUrls: ['./page.component.scss']
 })
 
-export class FavoriteComponent { }
+export class FavoriteComponent implements OnInit {
+  filter: string = '';
+  emojis: EmojiModel[] = [];
+
+  constructor(private emojiService: EmojiService) {}
+
+  trackBy(index: number, element: EmojiModel): string {
+    return element.name;
+  }
+
+  removeFavorite(name: string): void {
+    this.emojiService.setFavorite(name, false);
+  }
+
+  ngOnInit(): void {
+    this.emojiService.emojis.subscribe(data => {
+      this.emojis = data;
+    });
+  }
+}
