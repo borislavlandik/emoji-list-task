e2278a9cbe6f46dcc2f1758329dc59840b455eca
Борислав Ландик <borislavlandik00@gmail.com>
2021-01-02 14:51:08 +0800

    feat: add emoji list to removed page

--- src/app/removed.component.ts
+++ src/app/removed.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmojiModel} from './emoji.model';
+import {EmojiService} from './emoji.service';
 
 @Component({
   selector: 'removed-app',
-  template: '<h1>УДАЛЕННЫЕ</h1>'
+  template: `
+    <header class="header">
+      <h1 class="header__title">Удаленные</h1>
+      <app-search-box (onInput)="filter = $event"></app-search-box>
+    </header>
+    <ng-container *ngFor="let emoji of emojis | filter:'name':filter.toLowerCase(); index as i; trackBy: trackBy">
+      <app-emoji-item [emoji]="emoji" *ngIf="emoji.isDeleted">
+        <button actions (click)="restore(emoji.name)">
+          <img src="../assets/icons/restore.svg" alt="cross icon">
+        </button>
+      </app-emoji-item>
+    </ng-container>
+  `,
+  styleUrls: ['./page.component.scss']
 })
 
-export class RemovedComponent { }
+export class RemovedComponent implements OnInit {
+  filter: string = '';
+  emojis: EmojiModel[] = [];
+
+  constructor(private emojiService: EmojiService) {}
+
+  trackBy(index: number, element: EmojiModel): string {
+    return element.name;
+  }
+
+  restore(name: string): void {
+    this.emojiService.setDeleted(name, false);
+  }
+
+  ngOnInit(): void {
+    this.emojiService.emojis.subscribe(data => {
+      this.emojis = data;
+    });
+  }
+}
